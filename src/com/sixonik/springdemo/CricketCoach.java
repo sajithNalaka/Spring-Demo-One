3e91078199bb918e2eb82d4b81b276f84c180c50
sajithNalaka <sajith8401@gmail.com>
2020-04-11 04:00:54 +0530

    Setter injection - Literal values

--- src/com/sixonik/springdemo/CricketCoach.java
+++ src/com/sixonik/springdemo/CricketCoach.java
@@ -4,6 +4,10 @@ public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
 	
+	// Injecting literal values from setter injection
+	private String emailAddress;
+	private String team;
+	
 	// no-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach - Inside no-arg constructor");
@@ -13,6 +17,23 @@ public class CricketCoach implements Coach {
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("CricketCoach - Inside setter method - setFortuneService");
 		this.fortuneService = fortuneService;
+	}
+
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
 	}
 
 	@Override
--- src/com/sixonik/springdemo/SetterSpringApp.java
+++ src/com/sixonik/springdemo/SetterSpringApp.java
@@ -10,12 +10,15 @@ public class SetterSpringApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// Create the bean
-		Coach theCoach = context.getBean("myCricketCoach", Coach.class);
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
 		// call the methods of the bean
 		System.out.println(theCoach.getFullWorkout());
 		System.out.println(theCoach.getDailyFortune());
 		
+		//Get values - setter injection literal values
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
 		// close the context
 		context.close();
 	}
